b9e6c1d6d7f66735b2992247bff704df0210f842
Awom <liawom@foxmail.com>
2023-04-16 17:36:26 +0800

    feat: 🎸 新增isLogin状态判断

--- services/usercenter/fetchPerson.js
+++ services/usercenter/fetchPerson.js
@@ -1,4 +1,7 @@
 import { config } from '../../config/index';
+import { isLogin } from "../../utils/auth";
+import { delay } from "../_utils/delay";
+import { genSimpleUserInfo } from "../../model/usercenter";
 
 /** 获取个人中心信息 */
 function mockFetchPerson() {
@@ -17,12 +20,69 @@ function mockFetchPerson() {
   }));
 }
 
+
 /** 获取个人中心信息 */
 export function fetchPerson() {
+  if (!isLogin()) {
+    return delay().then(() => ({
+      avatarUrl:
+        'https://we-retail-static-1300977798.cos.ap-guangzhou.myqcloud.com/retail-ui/components-exp/avatar/avatar-1.jpg',
+      nickName: '111',
+      phoneNumber: '13438358888',
+      gender: 2,
+      address: {
+        provinceName: '',
+        provinceCode: '',
+        cityName: '',
+        cityCode: '',
+      },
+    }));
+  }
+
   if (config.useMock) {
     return mockFetchPerson();
   }
+
   return new Promise((resolve) => {
     resolve('real api');
   });
+}
+
+export function request(config) {
+  const instance = axios.create({
+    baseURL: 'http://127.0.0.1:8000',
+    timeout: 15000,
+    headers: {
+      'Authorization':  window.localStorage.getItem('token')
+    }
+  })
+
+  console.log("本次发送的token:", window.localStorage.getItem('token'))
+  instance.interceptors.request.use(config => {
+    return config;
+  }, error => {
+    console.log(error);
+  })
+
+  // 拦截响应
+  instance.interceptors.response.use(res => {
+    return res.data
+  }, error => {
+    console.log(error);
+  })
+
+  // 发送真正的网络请求
+  return instance(config);
+}
+
+
+export function login (userInfo) {
+  return request({
+    method: 'post',
+    url:    '/users/login',
+    params:   {
+      userName: userInfo.username,
+      password: userInfo.password
+    }
+  })
 }
---
+++ utils/auth.js
@@ -0,0 +1,30 @@
+const TokenKey = 'token';
+const UserInfoKey = 'userInfo';
+
+export function setUserInfo(obj) {
+  return wx.setStorageSync(UserInfoKey, obj);
+}
+export function getUserInfo() {
+  return wx.getStorageSync(UserInfoKey);
+}
+
+export function getToken() {
+  return wx.getStorageSync(TokenKey);
+}
+
+export function setToken(token) {
+  return wx.setStorageSync(TokenKey, token);
+}
+
+export function removeToken() {
+  return wx.removeStorageSync(TokenKey);
+}
+
+export function removeUserInfo() {
+  return wx.removeStorageSync(UserInfoKey);
+}
+
+export function isLogin() {
+  console.log("login:",getToken() != null && getToken() !== '' && typeof getToken() !== undefined)
+  return getToken() != null && getToken() !== '' && typeof getToken() !== undefined
+}
